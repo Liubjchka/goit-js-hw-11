87e8a21f9547eb3237afa1d27edce00698c138c2
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-02-20 19:25:58 +0200

    add hw-11

--- src/js/onFormSubmit.js
+++ src/js/onFormSubmit.js
@@ -2,7 +2,6 @@ import { refs } from './refs';
 import { pixabayApi } from './pixabayApi';
 import { beError } from './beError';
 import { watchLoader, offLoader } from './loader.js';
-// import { offLoader } from './loader.js';
 import { renderGallary } from './renderGallary';
 
 export async function onFormSubmit(event) {

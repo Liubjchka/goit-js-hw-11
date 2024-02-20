0d0b963d576d98ab458c428cfdb1db1dd8bcdb42
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-02-20 12:37:36 +0000

    Deploying to gh-pages from @ Liubjchka/goit-js-hw-11@f948c3b355c4d933d408c58eee916ee594971e88 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,16 +1,16 @@
-import"./assets/vendor-60237e46.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const c={container:document.querySelector(".container"),form:document.querySelector("#singleImagesForm"),btn:document.querySelector(".form-button"),loader:document.querySelector(".loader"),galleryList:document.querySelector(".gallery")};async function l(r){if(r.preventDefault(),refs.gallery.innerHTML="",userTag=r.target.elements.search.value.trim(),console.log(userTag),userTag===""){iziToast.error({icon:"",position:"bottomCenter",message:"Write something"});return}}function a(r){return`<li class="gallery-item">
-        <a href="${r.largeImageURL}" class="gallery-link">
+import{i as l,S as d}from"./assets/vendor-5b791d57.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();const s={container:document.querySelector(".container"),form:document.querySelector("#singleImagesForm"),btn:document.querySelector(".form-button"),loader:document.querySelector("#singleImages"),galleryList:document.querySelector(".gallery")};async function u(t){const o="42447990-17cd7de231c9689be9e26f0f6",i="https://pixabay.com/api/",n=`q=${t}&image_type=photo&orientation=horizontal&safesearch=true`,e=`${i}?key=${o}&${n}`;return await fetch(e).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}function c(){l.error({position:"bottomCenter",icon:"",message:"Sorry, there are no images matching your search query. Please try again!"})}function p(){s.loader.classList.remove("hidden")}function m(){s.loader.classList.add("hidden")}function f(t){return`<li class="gallery-item">
+        <a href="${t.largeImageURL}" class="gallery-link">
           <img
-            src="${r.webformatURL}"
+            src="${t.webformatURL}"
             class="gallery-image"
-            alt="${r.tags}"
+            alt="${t.tags}"
           />
         </a>
         <div class="description">
-            <div class="description-item"><p>Likes</p><span>${r.likes}</span></div>
-            <div class="description-item"><p>Downloads</p><span>${r.downloads}</span></div>
-            <div class="description-item"><p>Views</p><span>${r.views}</span></div>
-            <div class="description-item"><p>Comments</p><span>${r.comments}</span></div>
+            <div class="description-item"><p>Likes</p><span>${t.likes}</span></div>
+            <div class="description-item"><p>Downloads</p><span>${t.downloads}</span></div>
+            <div class="description-item"><p>Views</p><span>${t.views}</span></div>
+            <div class="description-item"><p>Comments</p><span>${t.comments}</span></div>
         </div>
-      </li>`}console.log(a);c.form.addEventListener("submit",l);
+      </li>`}function y(t){const o=t.hits.map(f).join("");if(t.hits.length){s.galleryList.innerHTML=o;const i=new d(".gallery a",{captions:!0,captionSelector:"img",captionPosition:"bottom",captionsData:"alt"});i.on("show.simplelightbox"),i.refresh()}else c()}function g(t){t.preventDefault(),p(),s.galleryList.innerHTML="";const o=t.currentTarget.elements.input.value.trim();u(o).then(y).catch(c).finally(m),s.form.reset()}s.form.addEventListener("submit",g);
 //# sourceMappingURL=commonHelpers.js.map

3a2a9f5b46c76e94f10f8eae80474a16372d6918
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-02-20 09:15:23 +0000

    Deploying to gh-pages from @ Liubjchka/goit-js-hw-11@ea0194fd7eb59612d0470504a22b2ab3e116878c 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,16 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import"./assets/vendor-60237e46.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const c={container:document.querySelector(".container"),form:document.querySelector("#singleImagesForm"),btn:document.querySelector(".form-button"),loader:document.querySelector(".loader"),galleryList:document.querySelector(".gallery")};async function l(r){if(r.preventDefault(),refs.gallery.innerHTML="",userTag=r.target.elements.search.value.trim(),console.log(userTag),userTag===""){iziToast.error({icon:"",position:"bottomCenter",message:"Write something"});return}}function a(r){return`<li class="gallery-item">
+        <a href="${r.largeImageURL}" class="gallery-link">
+          <img
+            src="${r.webformatURL}"
+            class="gallery-image"
+            alt="${r.tags}"
+          />
+        </a>
+        <div class="description">
+            <div class="description-item"><p>Likes</p><span>${r.likes}</span></div>
+            <div class="description-item"><p>Downloads</p><span>${r.downloads}</span></div>
+            <div class="description-item"><p>Views</p><span>${r.views}</span></div>
+            <div class="description-item"><p>Comments</p><span>${r.comments}</span></div>
+        </div>
+      </li>`}console.log(a);c.form.addEventListener("submit",l);
 //# sourceMappingURL=commonHelpers.js.map

2ec732db348f5a144d360b5b518b54c8e9524bb1
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-02-21 16:28:19 +0200

    add icon

--- src/js/beError.js
+++ src/js/beError.js
@@ -1,10 +1,11 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import '../img/Group.png';
 
 export function beError() {
   iziToast.error({
     position: 'topRight',
-    icon: '',
+    iconUrl: '../img/Group.png',
     maxWidth: 432,
     message:
       'Sorry, there are no images matching your search query.Please try again!',
